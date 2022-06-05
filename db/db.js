0a8d6ef63e298c6b4f40d1b8b19b103f1898dede
imabhi2000 <abhishekkumar1580472369@gmail.com>
2022-06-05 21:04:43 +0530

    second commit

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://imabhi:loveanjali@cluster0.2uce9kq.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    console.log('connection with database sucessfull..');
+    console.log('connection with Remote sucessfull..');
 }).catch((error)=>{
     console.log(error);
 }); 
