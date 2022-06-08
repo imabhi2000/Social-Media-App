9202c1368764f7e6d1d05b9432b1340d07caa574
imabhi2000 <abhishekkumar1580472369@gmail.com>
2022-06-08 22:07:50 +0530

    fourth commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongourl:'mongodb+srv://imabhi:loveanjali@cluster0.2uce9kq.mongodb.net/?retryWrites=true&w=majority',
+    googleclientid:"655567953739-i6a33ohs1c205nk901n1deo77i2o0utc.apps.googleusercontent.com",
+    googleclientsecret:"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"
+}
