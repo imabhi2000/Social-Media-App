27f816efc2b8feb2ae77c795e66140e651cd6a9b
imabhi2000 <abhishekkumar1580472369@gmail.com>
2022-06-09 20:26:44 +0530

    improoving facebook login

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     mongourl:'mongodb+srv://imabhi:loveanjali@cluster0.2uce9kq.mongodb.net/?retryWrites=true&w=majority',
     googleclientid:"655567953739-i6a33ohs1c205nk901n1deo77i2o0utc.apps.googleusercontent.com",
     googleclientsecret:"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",
-    facebookclientid:"419152509857753",
-    facebookclientsecret:"3cd1ba2581b4da137a224afbb82e031a",
+    facebookclientid:"5306185099442543",
+    facebookclientsecret:"b75f68ebf3022d118d0c3de53dc21fc3",
     instagramclientid:"540881580813439",
     instagramsecret:"25a33b4128f855bba5afe7be6206252d"
 }
