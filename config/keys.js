0d570a38b9134d9083a4d2ea4b871fe27c64d5b5
imabhi2000 <abhishekkumar1580472369@gmail.com>
2022-06-09 14:48:46 +0530

    sixth commit

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
     mongourl:'mongodb+srv://imabhi:loveanjali@cluster0.2uce9kq.mongodb.net/?retryWrites=true&w=majority',
     googleclientid:"655567953739-i6a33ohs1c205nk901n1deo77i2o0utc.apps.googleusercontent.com",
     googleclientsecret:"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",
-    facebookclientid:"1021829815110800",
-    facebookclientsecret:"be9cf5208271d719544dffb53b7ddb1f"
+    facebookclientid:"419152509857753",
+    facebookclientsecret:"3cd1ba2581b4da137a224afbb82e031a"
 }
